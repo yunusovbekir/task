5837f95e7e8ca5ba8d5b160660483f6b3b1bcbc2
Bakir Yunusov <yunusovbekir@gmail.com>
2021-09-30 18:16:39 +0200

    add models

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('make', 'model', 'avg_rating')
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('car', 'rating', 'review_datetime',)
+
+
+admin.site.register(Model)
+admin.site.register(Make)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Car, CarAdmin)
---
+++ app/core/models.py
@@ -0,0 +1,120 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Make(models.Model):
+    title = models.CharField(
+        _('title'),
+        max_length=255,
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _("Make")
+        verbose_name_plural = _("Makes")
+        ordering = ('title',)
+
+
+class Model(models.Model):
+    make = models.ForeignKey(
+        'core.Make',
+        on_delete=models.CASCADE,
+        verbose_name=_('make'),
+        related_name='models',
+    )
+    title = models.CharField(
+        _('title'),
+        max_length=255,
+    )
+
+    def __str__(self):
+        return f'{self.make.title} - {self.title}'
+
+    class Meta:
+        verbose_name = _("Model")
+        verbose_name_plural = _("Models")
+        ordering = ('title',)
+
+
+class RatingChoices(models.IntegerChoices):
+    VERY_BAD = 1
+    BAD = 2
+    POOR = 3
+    GOOD = 4
+    EXCELLENT = 5
+
+
+class Review(models.Model):
+    car = models.ForeignKey(
+        'core.Car',
+        on_delete=models.CASCADE,
+        verbose_name=_('car'),
+        related_name='reviews',
+    )
+    rating = models.IntegerField(
+        _('rating'),
+        choices=RatingChoices.choices,
+    )
+    review_datetime = models.DateTimeField(
+        _('review add datetime'),
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f"Review for {self.car}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        getattr(self.car, 'update_rating')()
+
+    class Meta:
+        verbose_name = _('Review')
+        verbose_name_plural = _('Reviews')
+        ordering = ('-review_datetime',)
+
+
+class Car(models.Model):
+    make = models.ForeignKey(
+        'core.Make',
+        on_delete=models.CASCADE,
+        verbose_name=_("make"),
+        related_name='cars',
+    )
+    model = models.ForeignKey(
+        'core.Model',
+        on_delete=models.CASCADE,
+        verbose_name=_("model"),
+        related_name='cars',
+    )
+    avg_rating = models.DecimalField(
+        _('average rating'),
+        default=1,
+        decimal_places=1,
+        max_digits=2,
+    )
+
+    def __str__(self):
+        return f"{self.make} - {self.model}"
+
+    class Meta:
+        verbose_name = _("Car")
+        verbose_name_plural = _("Cars")
+
+    def calculate_avg_rating(self):
+        """ Calculate average rating """
+        result = self.reviews.all().aggregate(
+            sum=models.Sum('rating'),
+            count=models.Count('id'),
+        )
+        reviews_sum = result.get('sum', 0)
+        reviews_count = result.get('count', 0)
+        rating = 0
+        if reviews_count > 0:
+            rating = float(reviews_sum) / reviews_count
+        return rating
+
+    def update_rating(self):
+        self.avg_rating = self.calculate_avg_rating()
+        self.save()
